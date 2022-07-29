14f429293fcf5871f5f01cec17a2a8480438d1a7
Wong Lok <wonglok@users.noreply.github.com>
2022-07-29 11:17:04 +0800

    sheidl

--- src/vfx-library/TheVortex/NoodleO3.js
+++ src/vfx-library/TheVortex/NoodleO3.js
@@ -343,7 +343,7 @@ export class LokLokWiggleSimulation {
           // positionChain.y += (cnoise(positionHead.rgb * 0.01 + 0.2)) * 0.4;
           // positionChain.z += (cnoise(positionHead.rgb * 0.01 + 0.3)) * 0.4;
 
-          positionChain.xyz *= 1.0 - 0.25 * 0.0135;
+          positionChain.xyz *= 1.0 + 0.25 * 0.0135;
 
           positionChain.xyz = positionChain.xyz + trackerPos;
 
