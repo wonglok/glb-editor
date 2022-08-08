7ec8236da071e6a76c1e4de58050d9b8dcd94d7e
Wong Lok <wonglok@users.noreply.github.com>
2022-08-08 17:28:03 +0800

    hold up!

--- src/aws/btn.tester.js
+++ src/aws/btn.tester.js
@@ -1,45 +1,47 @@
 import { removeS3, uploadS3 } from './aws.s3.gui'
 import { onReady } from '@/vfx-cms/firebase'
+import { useMetaStore } from '@/vfx-meta/store/use-meta-store'
 export default function S3BtnTester() {
   return (
     <div>
-      <button
-        onClick={() => {
-          let input = document.createElement('input')
-          input.type = 'file'
-          input.onchange = async ({ target: { files } }) => {
-            let file = files[0]
-            console.log(files)
+      {
+        <button
+          onClick={() => {
+            let input = document.createElement('input')
+            input.type = 'file'
+            input.onchange = async ({ target: { files } }) => {
+              let file = files[0]
+              console.log(files)
 
-            if (file) {
-              try {
-                //
-                let result = await uploadS3({
-                  file,
-                  idToken: '',
-                  folderPath: 'tester-folder',
-                })
+              if (file) {
+                try {
+                  //
+                  let result = await uploadS3({
+                    file,
+                    idToken: '',
+                    folderPath: 'tester-folder',
+                  })
 
-                //
+                  //
+                  let result2 = await removeS3({
+                    idToken: '',
+                    fileS3: result.fileInfo,
+                  })
 
-                let result2 = await removeS3({
-                  idToken: '',
-                  fileS3: result.fileInfo,
-                })
-
-                console.log(result2)
-                //
-              } catch (e) {
-                console.log(e)
+                  console.log(result2)
+                  //
+                } catch (e) {
+                  console.log(e)
+                }
               }
             }
-          }
 
-          input.click()
-        }}
-      >
-        upload
-      </button>
+            input.click()
+          }}
+        >
+          upload
+        </button>
+      }
     </div>
   )
 }
