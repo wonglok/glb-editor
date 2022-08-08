e12fb84bb4d53eef82e42f0b083460c0bc34792e
Wong Lok <wonglok@users.noreply.github.com>
2022-08-08 20:12:42 +0800

    bambam

--- src/vfx-meta/online/AvatarButton.js
+++ src/vfx-meta/online/AvatarButton.js
@@ -2,18 +2,22 @@ import Router from 'next/router'
 
 export function AvatarButton({}) {
   return (
-    <div
-      style={{
-        backdropFilter: 'blur(20px)',
-        borderRadius: '30px',
-        boxShadow: '0px 0px 30px 0px rgba(0,0,255,0.5)',
-      }}
-      onClick={() => {
-        Router.push(`/clothes`)
-      }}
-      className='inline-block w-full px-3 py-2 mb-4 text-center text-white bg-blue-500 cursor-pointer rounded-xl'
-    >
-      Avatar
-    </div>
+    <>
+      {Router.pathname !== `/clothes` && (
+        <div
+          style={{
+            backdropFilter: 'blur(20px)',
+            borderRadius: '30px',
+            boxShadow: '0px 0px 30px 0px rgba(0,0,255,0.5)',
+          }}
+          onClick={() => {
+            Router.push(`/clothes`)
+          }}
+          className='inline-block w-full px-3 py-2 mb-4 text-center text-white bg-blue-500 cursor-pointer rounded-xl'
+        >
+          Change Avatar
+        </div>
+      )}
+    </>
   )
 }
--- src/vfx-meta/online/HomeButton.js
+++ src/vfx-meta/online/HomeButton.js
@@ -2,18 +2,21 @@ import Router from 'next/router'
 
 export function HomeButton({}) {
   return (
-    <div
-      style={{
-        backdropFilter: 'blur(20px)',
-        borderRadius: '30px',
-        boxShadow: '0px 0px 30px 0px rgba(0,0,255,0.5)',
-      }}
-      onClick={() => {
-        Router.push(`/`)
-      }}
-      className='inline-block w-full px-3 py-2 mb-4 text-center text-white bg-blue-500 cursor-pointer rounded-xl'
-    >
-      Home
-    </div>
+    Router.pathname !== '/' &&
+    ((
+      <div
+        style={{
+          backdropFilter: 'blur(20px)',
+          borderRadius: '30px',
+          boxShadow: '0px 0px 30px 0px rgba(0,0,255,0.5)',
+        }}
+        onClick={() => {
+          Router.push(`/`)
+        }}
+        className='inline-block w-full px-3 py-2 mb-4 text-center text-white bg-blue-500 cursor-pointer rounded-xl'
+      >
+        Home
+      </div>
+    ) || <></>)
   )
 }
