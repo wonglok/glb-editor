3605c8af16589015b5a56f0584a55a5890483993
Wong Lok <wonglok@users.noreply.github.com>
2022-08-10 17:18:14 +0800

    yozone

--- src/vfx-meta/game/Chatroom.js
+++ src/vfx-meta/game/Chatroom.js
@@ -1,7 +1,8 @@
 // import { UIContent } from '@/vfx-core/UIContent'
 // import { ClosetBtns } from '../game-parts/ClosetBtns'
 import { UIContent } from '@/vfx-core/UIContent'
-import { ContactShadows } from '@react-three/drei'
+import { ContactShadows, Text } from '@react-three/drei'
+import { Suspense } from 'react'
 import { Conf } from '../game-parts/Conf'
 import { Floor } from '../game-parts/Floor'
 import { HDR } from '../game-parts/HDR'
@@ -12,24 +13,31 @@ import { OnlineSystem } from '../online/OnlineSystem'
 export function Chatroom() {
   return (
     <group>
-      <Floor url={'/scene/chatroom/chat-room-v2.glb'}></Floor>
-      <HDR></HDR>
-      {/*  */}
+      <Suspense
+        fallback={
+          <Text color={'black'} fontSize={0.25}>
+            Loading...
+          </Text>
+        }
+      >
+        <Floor url={'/scene/chatroom/chat-room-v2.glb'}></Floor>
+        <HDR></HDR>
+        {/*  */}
 
-      <OnlineSystem mapID='/scene/chatroom/chat-room-v2.glb'>
-        <>
-          <Player></Player>
+        <OnlineSystem mapID='/scene/chatroom/chat-room-v2.glb'>
+          <>
+            <Player></Player>
 
-          <Effects></Effects>
+            <Effects></Effects>
 
-          <UIContent>
-            <div className=' absolute top-0 left-0 w-96 '>
-              <Conf></Conf>
-            </div>
-          </UIContent>
-        </>
-      </OnlineSystem>
-
+            <UIContent>
+              <div className=' absolute top-0 left-0 w-96 '>
+                <Conf></Conf>
+              </div>
+            </UIContent>
+          </>
+        </OnlineSystem>
+      </Suspense>
       {/*  */}
       {/* <ClosetBtns></ClosetBtns> */}
     </group>
