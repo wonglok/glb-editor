7d7f04660da06a871704763b9b0cac703691df81
Wong Lok <wonglok@users.noreply.github.com>
2022-08-12 10:45:10 +0800

    yk

--- src/vfx-studio/UI3D/SceneContent.js
+++ src/vfx-studio/UI3D/SceneContent.js
@@ -25,7 +25,7 @@ export function SceneContent({}) {
   return (
     <>
       {/*  */}
-      {glbObject && glbObjectForEffect && (
+      {glbObject && (
         <>
           <Select
             box
