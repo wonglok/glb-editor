9847202a152228a0ac76c5b0c6a992c2fadbb895
Wong Lok <wonglok@users.noreply.github.com>
2022-08-13 07:53:17 +0800

    yo

---
+++ src/vfx-nodes/post-processing-v0/pp.v0.pass.hue-satuation.en.js
@@ -0,0 +1,94 @@
+import { getID } from '@/vfx-runtime/ENUtils'
+import { HueSaturation } from '@react-three/postprocessing'
+
+let props = {
+  hue: 0,
+  saturation: 0,
+}
+
+export async function nodeData({ defaultData, nodeID }) {
+  let uniforms = []
+  let getType = (value) => {
+    if (typeof value === 'undefined') {
+      return 'bool'
+    } else if (typeof value === 'boolean') {
+      return 'bool'
+    } else if (typeof value === 'string') {
+      return 'text'
+    } else if (typeof value === 'number') {
+      return 'float'
+    } else if (value === null) {
+      return 'bool'
+    }
+  }
+  for (let kn in props) {
+    let newItem = {
+      _id: getID(),
+      nodeID,
+      name: kn,
+      type: getType(props[kn]),
+      value: props[kn],
+    }
+    uniforms.push(newItem)
+  }
+
+  return {
+    ...defaultData,
+
+    //
+    //
+    inputs: [
+      //
+      { _id: getID(), type: 'input', nodeID },
+    ],
+
+    // at least 1
+    //
+    outputs: [
+      //
+      { _id: getID(), type: 'output', nodeID },
+    ],
+
+    //
+    material: [],
+
+    uniforms: [...uniforms],
+
+    //
+    shaders: [],
+
+    //
+  }
+}
+
+export function effect({ node, mini, data, setComponent }) {
+  let defaultConfig = {}
+  let send = () => {
+    node.out0.pulse(<HueSaturation key={getID()} {...defaultConfig} />)
+  }
+
+  let keys = Object.keys(props)
+  for (let key of keys) {
+    defaultConfig[key] = data.value[key]
+  }
+
+  let needsUpdate = true
+  for (let key of keys) {
+    data.uniforms[key]((signal) => {
+      defaultConfig[key] = signal.value
+      needsUpdate = true
+    })
+  }
+
+  let tt = setInterval(() => {
+    if (needsUpdate) {
+      needsUpdate = false
+      send()
+    }
+  }, 1)
+  mini.onClean(() => {
+    clearInterval(tt)
+  })
+
+  //
+}
