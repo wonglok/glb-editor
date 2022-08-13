2e480164e3e38189e1b48589041b022d4eeb1c90
Wong Lok <wonglok@users.noreply.github.com>
2022-08-13 08:16:54 +0800

    wooohooo

--- src/vfx-studio/UI3D/SceneContent_Orbit.js
+++ src/vfx-studio/UI3D/SceneContent_Orbit.js
@@ -2,12 +2,7 @@ import { useRef } from 'react'
 import { useAccessor } from '@/vfx-studio/store/use-accessor'
 import { useThree } from '@react-three/fiber'
 import { HDRTex } from './HDRTex'
-import {
-  FlyControls,
-  MapControls,
-  OrbitControls,
-  Select,
-} from '@react-three/drei'
+import { OrbitControls, Select } from '@react-three/drei'
 import { BoxHelper, Object3D, Vector3 } from 'three'
 import anime from 'animejs'
 import { EffectNodeRuntime } from '../effectnode/Runtime/EffectNodeRuntime/EffectNodeRuntime'
@@ -44,52 +39,53 @@ export function SceneContent({}) {
                 openEffectNode(v[0])
                 setLayout('effectnode')
 
-                // v[0].geometry.computeBoundingSphere()
-                // let center = v[0].geometry.boundingSphere.center
-                // let radius = v[0].geometry.boundingSphere.radius
+                v[0].geometry.computeBoundingSphere()
+                let center = v[0].geometry.boundingSphere.center
+                let radius = v[0].geometry.boundingSphere.radius
 
-                // //
-                // let next = new Vector3()
-                // next.copy(center)
-                // v[0].updateMatrixWorld()
-                // next.applyMatrix4(v[0].matrixWorld)
+                //
+                let next = new Vector3()
+                next.copy(center)
+                v[0].updateMatrixWorld()
+                next.applyMatrix4(v[0].matrixWorld)
 
-                // let diff = control.object.position
-                //   .clone()
-                //   .sub(control.target)
-                //   .normalize()
-                //   .multiplyScalar(radius * 2 + 1)
+                let diff = control.object.position
+                  .clone()
+                  .sub(control.target)
+                  .normalize()
+                  .multiplyScalar(radius * 2 + 1)
 
-                // let posFinal = next.clone().add(diff)
+                let posFinal = next.clone().add(diff)
 
-                // let anim = anime({
-                //   targets: [control.object.position],
-                //   x: posFinal.x,
-                //   y: posFinal.y,
-                //   z: posFinal.z,
-                //   duration: 500,
-                //   easing: 'easeInOutQuad',
-                // })
+                let anim = anime({
+                  targets: [control.object.position],
+                  x: posFinal.x,
+                  y: posFinal.y,
+                  z: posFinal.z,
+                  duration: 500,
+                  easing: 'easeInOutQuad',
+                })
 
-                // let anim2 = anime({
-                //   targets: [control.target],
-                //   x: next.x,
-                //   y: next.y,
-                //   z: next.z,
-                //   duration: 500,
-                //   easing: 'easeInOutQuad',
-                // })
+                let anim2 = anime({
+                  targets: [control.target],
+                  x: next.x,
+                  y: next.y,
+                  z: next.z,
+                  duration: 500,
+                  easing: 'easeInOutQuad',
+                })
 
-                // let h = () => {
-                //   window.removeEventListener('wheel', h)
-                //   anim.pause()
-                //   anim2.pause()
-                // }
-                // window.addEventListener('wheel', h)
+                let h = () => {
+                  window.removeEventListener('wheel', h)
+                  anim.pause()
+                  anim2.pause()
+                }
+                window.addEventListener('wheel', h)
+
+                clean.current()
 
                 let helper = new BoxHelper(v[0])
 
-                clean.current()
                 clean.current = () => {
                   helper.removeFromParent()
                 }
@@ -114,7 +110,7 @@ export function SceneContent({}) {
 
       <HDRTex scene={scene} url={`/hdr/greenwich_park_02_1k.hdr`} />
 
-      {/* <OrbitControls
+      <OrbitControls
         ref={(ev) => {
           setContorl(ev)
         }}
@@ -122,20 +118,7 @@ export function SceneContent({}) {
         target-y={0}
         object-position-z={3}
         makeDefault={true}
-      ></OrbitControls> */}
-
-      <MapControls
-        object-position-y={3}
-        object-position-z={3}
-        target-x={0}
-        target-y={0}
-        target-z={0}
-        enableZoom={true}
-        ref={(ev) => {
-          setContorl(ev)
-        }}
-        enableRotate={false}
-      />
+      ></OrbitControls>
     </>
   )
 }
