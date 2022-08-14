1e7187bb6c21911f288497694ec6de2fbfd99062
Wong Lok <wonglok@users.noreply.github.com>
2022-08-14 08:57:46 +0800

    how much more

--- src/vfx-meta/game/ChangeClothes.js
+++ src/vfx-meta/game/ChangeClothes.js
@@ -1,28 +1,67 @@
 // import { UIContent } from '@/vfx-core/UIContent'
+import { UIContent } from '@/vfx-core/UIContent'
+import { useEffect } from 'react'
+import { BG } from '../game-parts/BG'
 import { ClothesUI } from '../game-parts/ClothesUI'
 import { Floor } from '../game-parts/Floor'
 import { HDR } from '../game-parts/HDR'
 import { Player } from '../game-parts/Player'
 import { Effects } from '../game-vfx/Effects'
+import { EnvLight } from '../game-vfx/EnvLight'
 import { OnlineSystem } from '../online/OnlineSystem'
+import { useRender } from '../store/use-render'
 
 export function ChangeClothes() {
   return (
     <group>
       <Floor url={'/scene/closet/closet.glb'}></Floor>
-      <HDR></HDR>
+      {/* <HDR></HDR> */}
+      <EnvLight></EnvLight>
+      <BG url={`/hdr/moonless_golf_1k.hdr`}></BG>
+
       {/*  */}
 
       <OnlineSystem mapID='closet'>
         <>
           <Player></Player>
           <ClothesUI></ClothesUI>
-          {/* <Effects></Effects> */}
+          <Effects></Effects>
         </>
       </OnlineSystem>
 
       {/*  */}
+
+      <TopLeft></TopLeft>
     </group>
+  )
+}
+
+//
+
+function TopLeft() {
+  const enable = useRender((s) => s.enable)
+  const setRender = useRender((s) => s.setRender)
+
+  //
+  useEffect(() => {
+    setRender(true)
+  }, [])
+  return (
+    <>
+      <UIContent>
+        <div className='absolute top-0 left-0'>
+          <button
+            className='p-3 m-3 bg-white rounded-lg'
+            onClick={() => {
+              //
+              setRender(!enable)
+            }}
+          >
+            Toggle Render
+          </button>
+        </div>
+      </UIContent>
+    </>
   )
 }
 
